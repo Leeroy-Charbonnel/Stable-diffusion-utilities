b42e91d9e2f5f8b5dddb77ac18fe55737e9516c6
leeroy dylan <leeroydylan0@gmail.com>
2025-05-09 14:03:56 +0200

    Update 09/05/2025 - 14:03

--- src/components/part-prompt/PromptsManager.tsx
+++ src/components/part-prompt/PromptsManager.tsx
@@ -1,11 +1,1 @@
-// src/components/part-prompt/PromptsManager.tsx
-import React from 'react';
-import { NodeSystem } from './custom-nodes/NodeSystem';
-
-export function PromptsManager() {
-  return (
-    <div className="h-full w-full">
-      <NodeSystem />
-    </div>
-  );
-}
+HERE GOES THE PROMPT MANAGER CODE
